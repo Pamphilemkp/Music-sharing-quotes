1288225daf78a2d7d69053c523c76718e638aade
pamphilemkp <pamphilemkp@gmail.com>
2022-06-27 23:13:19 +0300

    setup linters and and fix errors

--- app.js
+++ app.js
@@ -1,18 +1,16 @@
-const mobileMenu = document.querySelector(".mobile-menu");
-const OpenMenu = document.querySelector(".hamburger");
-const CloseMenu = document.querySelector(".close_mobile_menu");
-
+const mobileMenu = document.querySelector('.mobile-menu');
+const OpenMenu = document.querySelector('.hamburger');
+const CloseMenu = document.querySelector('.close_mobile_menu');
 
 function OpenHamburger() {
-    mobileMenu.classList.toggle("open-menu");
-    mobileMenu.style.display = 'block';
-
+  mobileMenu.classList.toggle('open-menu');
+  mobileMenu.style.display = 'block';
 }
 
 function CloseHamburger() {
-    mobileMenu.classList.remove("open-menu");
-    mobileMenu.style.display = 'none';
+  mobileMenu.classList.remove('open-menu');
+  mobileMenu.style.display = 'none';
 }
 
 OpenMenu.addEventListener('click', OpenHamburger);
-CloseMenu .addEventListener('click', CloseHamburger);
+CloseMenu.addEventListener('click', CloseHamburger);
